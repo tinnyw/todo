666cf955659a9cc276e5430841f91a4ed32478f8
Tinny Washington <rwashington@salesforce.com>
2017-05-24 01:47:17 -0400

    get all

--- api/main.go
+++ api/main.go
@@ -23,6 +23,7 @@ func main() {
 	})*/
 	router := mux.NewRouter()
 	router.HandleFunc("/", Index).Methods("GET")
+	router.HandleFunc("/getAllTodos", GetAllTodos).Methods("GET")
 	router.HandleFunc("/newTodo", CreateTodo).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":80", router))
@@ -33,10 +34,22 @@ func Index(w http.ResponseWriter, req *http.Request) {
 }
 
 
+func GetAllTodos(w http.ResponseWriter, req *http.Request) {
+
+	var todoItems = querySql("SELECT id, value, checked FROM todo")
+	todoJson, err := json.Marshal(todoItems)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+ 	w.Write(todoJson)
+}
+
+
 func CreateTodo(w http.ResponseWriter, req *http.Request) {
 	todoItems := DecodeTodoRequest(req)
-	// var todoItem Todo
-	// _ = json.NewDecoder(req.Body).Decode(&todoItem)
+
 	for _, todoItem := range todoItems {
 		log.Print("Todo params are:" + todoItem.Value)
 		executeSql("INSERT INTO todo (value) values ('" + todoItem.Value + "')")
@@ -45,8 +58,6 @@ func CreateTodo(w http.ResponseWriter, req *http.Request) {
 
 func DecodeTodoRequest(req *http.Request) (todoItems []Todo) {
 	decoder := json.NewDecoder(req.Body)
-	/*var todoItems = new (Todo)
-  err := json.Unmarshal(req.Body, &todoItems)*/
 
 	err := decoder.Decode(&todoItems)
 	if err != nil {
@@ -70,4 +81,33 @@ func executeSql(stmnt string) {
 		log.Panic(err);
 		log.Fatal("Error: SQL execution messed up")
 	}
+}
+
+func querySql(query string) (todoItems []Todo) {
+	db, err := sql.Open("mysql", "root:bob@tcp(db:3306)/todo")
+	if err != nil {
+ 		log.Panic(err);
+		log.Fatal("Error: Connection to the DB messed up")
+	}
+	defer db.Close()
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Panic(err);
+		log.Fatal("Error: SQL query messed up")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var todoItem Todo
+		rows.Scan(&todoItem.ID, &todoItem.Value, &todoItem.Checked)
+		todoItems = append(todoItems, todoItem)
+	}
+
+	if err != nil {
+		log.Panic(err);
+		log.Fatal("Error: SQL execution messed up")
+	}
+
+	return
 }
