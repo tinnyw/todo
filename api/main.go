da919237970850f992b03b8ce00682184d7f7264
Tinny Washington <rwashington@salesforce.com>
2017-05-24 02:18:08 -0400

    check and uncheck

--- api/main.go
+++ api/main.go
@@ -9,6 +9,7 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"encoding/json"
+	"strconv"
 )
 
 type Todo  struct {
@@ -17,25 +18,25 @@ type Todo  struct {
 	Checked bool `json:"checked"`
 }
 
+// handle all the different API routes
 func main() {
-	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "LOGOS, i <3 YOU!!!!")
-	})*/
 	router := mux.NewRouter()
 	router.HandleFunc("/", Index).Methods("GET")
 	router.HandleFunc("/getAllTodos", GetAllTodos).Methods("GET")
-	router.HandleFunc("/newTodo", CreateTodo).Methods("POST")
+	router.HandleFunc("/createTodos", CreateTodos).Methods("POST")
+	router.HandleFunc("/checkTodos", CheckTodos).Methods("POST")
+	router.HandleFunc("/uncheckTodos", UncheckTodos).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":80", router))
 }
 
+// main page for the API service
 func Index(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hey Brendan!")
 }
 
-
+// get all the todos
 func GetAllTodos(w http.ResponseWriter, req *http.Request) {
-
 	var todoItems = querySql("SELECT id, value, checked FROM todo")
 	todoJson, err := json.Marshal(todoItems)
 	if err != nil {
@@ -46,13 +47,31 @@ func GetAllTodos(w http.ResponseWriter, req *http.Request) {
  	w.Write(todoJson)
 }
 
-
-func CreateTodo(w http.ResponseWriter, req *http.Request) {
+// create a list of new todos
+func CreateTodos(w http.ResponseWriter, req *http.Request) {
 	todoItems := DecodeTodoRequest(req)
 
 	for _, todoItem := range todoItems {
 		log.Print("Todo params are:" + todoItem.Value)
 		executeSql("INSERT INTO todo (value) values ('" + todoItem.Value + "')")
+	}
+}
+
+func CheckTodos(w http.ResponseWriter, req *http.Request) {
+	todoItemsToCheck := DecodeTodoRequest(req)
+
+	for _, todoItemToCheck := range todoItemsToCheck {
+		log.Print("Todo params are:" + todoItemToCheck.Value)
+		executeSql("UPDATE todo SET checked = 1 WHERE id = " + strconv.Itoa(todoItemToCheck.ID))
+	}
+}
+
+func UncheckTodos(w http.ResponseWriter, req *http.Request) {
+	todoItemsToUncheck := DecodeTodoRequest(req)
+
+	for _, todoItemToUncheck := range todoItemsToUncheck {
+		log.Print("Todo params are:" + todoItemToUncheck.Value)
+		executeSql("UPDATE todo SET checked = 0 WHERE id = " + strconv.Itoa(todoItemToUncheck.ID))
 	}
 }
 
